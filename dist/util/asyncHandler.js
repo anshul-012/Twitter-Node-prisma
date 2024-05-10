95c045f9134100b79c26afcd66d6ff0cbefa9c07
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-05-11 01:32:32 +0530

    fixed

--- dist/util/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (passedFunction) => async (req, res, next) => {
-    return Promise
-        .resolve(passedFunction(req, res, next))
-        .catch((err) => next(err));
-};
-exports.default = asyncHandler;
